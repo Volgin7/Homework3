e5f00db212d9e1cac37c5edc13d75470c4f4e0f0
Volgin7 <8612020@gmail.com>
2023-02-15 13:02:22 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+
+        System.out.println("Задача 1");
+
+        int varInt = 1234;
+        System.out.println("Значение переменной varInt с типом int равно " + varInt);
+
     }
 }

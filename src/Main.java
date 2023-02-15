83aa34b8f9220f2de2c80fa4e46f4d210df7aecd
Volgin7 <8612020@gmail.com>
2023-02-15 13:12:42 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,18 @@ public class Main {
 
         System.out.println("Задача 1");
 
-        int varInt = 1234;
+        int varInt = 12345;
         System.out.println("Значение переменной varInt с типом int равно " + varInt);
+        byte varByte = 123;
+        System.out.println("Значение переменной varByte с типом byte равно " + varByte);
+        short varShort = 1234;
+        System.out.println("Значение переменной varShort с типом short равно " + varShort);
+        long varLong = 123456L;
+        System.out.println("Значение переменной varLong с типом long равно " + varLong);
+        float varFloat = 123.456f;
+        System.out.println("Значение переменной varFloat с типом float равно " + varFloat);
+        double varDouble = 123.4567899999999;
+        System.out.println("Значение переменной varDouble с типом double равно " + varDouble);
 
     }
 }
